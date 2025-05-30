7306ea604a17c5155371dcd6117bc1becdc82039
CHARLES DAVIS <charles@mediavii.com>
2025-05-30 21:09:37 +0000

    The NextJS app server is having trouble starting. Please identify what could be wrong and if there's an error, try fixing the problem based on the following server logs:
    > 2025-05-30T21:07:43Z [web] <IDX> Starting preview...
    > 2025-05-30T21:07:43Z [web] Waiting for your web server to start serving
    > 2025-05-30T21:07:46Z [web]
    > 2025-05-30T21:07:46Z [web] > nextn@0.1.0 dev
    > 2025-05-30T21:07:46Z [web] > next dev --turbopack -p 9002 --port 9002 --hostname 0.0.0.0
    > 2025-05-30T21:07:46Z [web]
    > 2025-05-30T21:07:47Z [web] <IDX> Starting preview...
    > 2025-05-30T21:07:47Z [web] Waiting for your web server to start serving
    > 2025-05-30T21:07:47Z [web]
    > 2025-05-30T21:07:47Z [web] > nextn@0.1.0 dev
    > 2025-05-30T21:07:47Z [web] > next dev --turbopack -p 9002 --port 9002 --hostname 0.0.0.0
    > 2025-05-30T21:07:47Z [web]
    > 2025-05-30T21:07:51Z [web]    ▲ Next.js 15.2.3 (Turbopack)
    > 2025-05-30T21:07:51Z [web]    - Local:        http://localhost:9002
    > 2025-05-30T21:07:51Z [web]    - Network:      http://0.0.0.0:9002
    > 2025-05-30T21:07:51Z [web]    - Environments: .env
    > 2025-05-30T21:07:51Z [web]
    > 2025-05-30T21:07:51Z [web]  ✓ Starting...
    > 2025-05-30T21:07:51Z [web] <IDX> ▶️ Preview running
    > 2025-05-30T21:07:57Z [web]  ✓ Ready in 6s
    > 2025-05-30T21:07:58Z [web]  ○ Compiling / ...

---
+++ src/next.config.ts
@@ -0,0 +1,96 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.columbiarestaurant.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.shopinternationalplaza.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.pwc.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.simpleviewinc.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.usf.edu',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.smithandassociates.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.fermanauto.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'editionhotels.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.connectwise.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'moffitt.org',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
